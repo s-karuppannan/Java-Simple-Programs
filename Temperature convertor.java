a484c35eff3653fe25f1d1883362cde9273c2afe
karuppannan <karuppannan00001@gmail.com>
2024-09-11 10:30:34 +0530

    Temperature convertor.java

--- Temperature convertor.java
+++ Temperature convertor.java
@@ -1,1 +1,19 @@
-
+import java.util.Scanner;
+public class TemperatureConverter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter temperature: ");
+        double temp = sc.nextDouble();
+        System.out.print("Convert to (C/F): ");
+        char unit = sc.next().toUpperCase().charAt(0);
+        if (unit == 'C') {
+            double celsius = (temp - 32) * 5 / 9;
+            System.out.println("Temperature in Celsius: " + celsius);
+        } else if (unit == 'F') {
+            double fahrenheit = (temp * 9 / 5) + 32;
+            System.out.println("Temperature in Fahrenheit: " + fahrenheit);
+        } else {
+            System.out.println("Invalid unit");
+        }
+    }
+}
