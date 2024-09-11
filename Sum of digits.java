dc12b6a29bc8fedc3794f92d8cebc237906d0bc6
karuppannan <karuppannan00001@gmail.com>
2024-09-11 10:23:36 +0530

    Sum of digits.java

--- Sum of digits.java
+++ Sum of digits.java
@@ -4,31 +4,22 @@ public class SumOfDigits {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Input integer from user
         System.out.print("Enter an integer: ");
         int number = scanner.nextInt();
 
-        // Calculate the sum of digits
         int sum = calculateSumOfDigits(number);
-
-        // Output the result
         System.out.println("The sum of the digits of " + number + " is: " + sum);
-
         scanner.close();
     }
 
-    // Method to calculate the sum of digits of an integer
     public static int calculateSumOfDigits(int num) {
         int sum = 0;
-        
-        // Convert negative numbers to positive
         num = Math.abs(num);
 
         while (num > 0) {
-            sum += num % 10; // Add the last digit to the sum
-            num /= 10; // Remove the last digit
+            sum += num % 10; 
+            num /= 10;
         }
-
         return sum;
     }
-}￼Enter
+}
